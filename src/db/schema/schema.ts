def6302a0d02bbf855cba3f05f6962993c9abbde
shiorioz <ozws011.tmt@gmail.com>
2024-11-15 06:47:51 +0000

    [add] refs 8 クイズログテーブル追加・マイグレーション

--- src/db/schema/schema.ts
+++ src/db/schema/schema.ts
@@ -1,11 +1,13 @@
-import { relations } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 import {
   boolean,
   char,
+  check,
   date,
   index,
   int,
   mysqlTable,
+  text,
   varchar,
 } from 'drizzle-orm/mysql-core';
 import { timestamps } from './columns.helpers';
@@ -43,6 +45,24 @@ export const quizSetLogTable = mysqlTable('quiz_set_log', {
   ...timestamps,
 });
 
+export const quizLogTable = mysqlTable(
+  'quiz_log',
+  {
+    id: char({ length: 36 }).primaryKey(),
+    quiz_id: char({ length: 36 }).notNull(),
+    quiz_set_log_id: char({ length: 36 })
+      .notNull()
+      .references(() => quizSetLogTable.id),
+    user_answer: text().notNull(),
+    time: int(),
+    is_correct: boolean().notNull(),
+    ...timestamps,
+  },
+  (table) => ({
+    timeCheck: check('time_check', sql`${table.time} >= 0`),
+  })
+);
+
 export const quizModeTable = mysqlTable('quiz_mode', {
   id: char({ length: 36 }).primaryKey(),
   name: varchar({ length: 20 }).notNull(),
@@ -62,18 +82,26 @@ export const userCostumesTable = mysqlTable('user_costumes', {
 /**
  * リレーション
  */
-export const userRelations = relations(usersTable, ({ one, many }) => ({
-  quizSetLog: one(quizSetLogTable),
-  userCostume: many(userCostumesTable),
+export const userRelations = relations(usersTable, ({ many }) => ({
+  quizSetLogs: many(quizSetLogTable),
+  userCostumes: many(userCostumesTable),
 }));
 
-export const quizSetLogRelations = relations(quizSetLogTable, ({ one }) => ({
-  users: one(usersTable),
-  quizMode: one(quizModeTable),
+export const quizSetLogRelations = relations(
+  quizSetLogTable,
+  ({ one, many }) => ({
+    user: one(usersTable),
+    quizMode: one(quizModeTable),
+    quizLogs: many(quizLogTable),
+  })
+);
+
+export const quizLogRelations = relations(quizLogTable, ({ one }) => ({
+  quizSetLog: one(quizSetLogTable),
 }));
 
-export const quizModeRelations = relations(quizModeTable, ({ one }) => ({
-  quizSetLog: one(quizSetLogTable),
+export const quizModeRelations = relations(quizModeTable, ({ many }) => ({
+  quizSetLogs: many(quizSetLogTable),
 }));
 
 export const userCostumesRelations = relations(
