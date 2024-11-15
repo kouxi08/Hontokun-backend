984fc2b04692781f2d6b03c406ee1a47e80d162d
shiorioz <ozws011.tmt@gmail.com>
2024-11-15 01:44:25 +0000

    [add] refs 8 ユーザのバリデーション追加

--- db/schema/users.ts
+++ db/schema/users.ts
@@ -13,7 +13,7 @@ export const usersTable = mysqlTable(
   {
     id: char({ length: 36 }).primaryKey(),
     firebase_uid: varchar({ length: 128 }).notNull().unique(),
-    nickname: varchar({ length: 50 }).notNull(),
+    nickname: varchar({ length: 20 }).notNull(),
     birthday: date(),
     level: int().default(1).notNull(),
     experience: int().default(0).notNull(),
