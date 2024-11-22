a514bd059f785311ae335fb2c2a02bbd8d8c8e5f
shiorioz <ozws011.tmt@gmail.com>
2024-11-22 07:00:06 +0000

    [add] refs 12 クイズのapiへの型変換・モデル定義を追加

---
+++ src/model/quiz/quiz.ts
@@ -0,0 +1,44 @@
+import { QuizId } from './quizId';
+
+export class Quiz {
+  private constructor(
+    private readonly _id: string,
+    private _news: {
+      title: string;
+      content: string;
+      image: string;
+    },
+    private _question: string,
+    private _type: 'true_or_false' | 'multiple_choice',
+    private _choices: string[] | null,
+    private _correctAnswer: string
+  ) {}
+
+  get id(): QuizId {
+    return this._id;
+  }
+
+  get news(): {
+    title: string;
+    content: string;
+    image: string;
+  } {
+    return this._news;
+  }
+
+  get question(): string {
+    return this._question;
+  }
+
+  get type(): 'true_or_false' | 'multiple_choice' {
+    return this._type;
+  }
+
+  get choices(): string[] | null {
+    return this._choices;
+  }
+
+  get correctAnswer(): string {
+    return this._correctAnswer;
+  }
+}
