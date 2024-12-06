48c8e220cae1315d116fe0b5bdfa004bae302d2f
shiorioz <ozws011.tmt@gmail.com>
2024-12-06 06:45:32 +0000

    [add] refs 50 エラーハンドリング修正

--- src/repository/quiz.ts
+++ src/repository/quiz.ts
@@ -12,40 +12,29 @@ export const createQuiz = async (
   db: MySql2Database,
   quiz: Quiz
 ): Promise<void> => {
-  try {
-    const validatedQuiz = insertQuizSchema.parse(quiz.toDatabaseObject());
+  const validatedQuiz = insertQuizSchema.parse(quiz.toDatabaseObject());
 
-    const choices = quiz.getChoices().map((choice) => {
-      return {
-        quiz_id: validatedQuiz.id,
-        name: choice,
-        created_at: validatedQuiz.created_at,
-        updated_at: validatedQuiz.updated_at,
-      };
-    });
-    const validatedChoice = choices.map((choice) => insertQuizChoiceSchema.parse(choice));
+  const choices = quiz.getChoices().map((choice) => {
+    return {
+      quiz_id: validatedQuiz.id,
+      name: choice,
+      created_at: validatedQuiz.created_at,
+      updated_at: validatedQuiz.updated_at,
+    };
+  });
+  const validatedChoice = choices.map((choice) => insertQuizChoiceSchema.parse(choice));
 
-    if (!validatedQuiz || !validatedChoice) {
-      throw new Error();
-    }
-
-    await db.transaction(async (trx) => {
-      await trx.insert(quizTable).values([validatedQuiz]);
-      await Promise.all(
-        validatedChoice.map((choice) => trx.insert(quizChoiceTable).values([choice]))
-      );
-    });
-
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      console.error('Zod Validation Error:', error.issues);
-      throw new Error('Zod Validation Error');
-    } else {
-      console.error('Failed to create quiz:', error);
-      throw new Error('Failed to create quiz');
-    }
+  if (!validatedQuiz || !validatedChoice) {
+    throw new Error();
   }
 
+  await db.transaction(async (trx) => {
+    await trx.insert(quizTable).values([validatedQuiz]);
+    await Promise.all(
+      validatedChoice.map((choice) => trx.insert(quizChoiceTable).values([choice]))
+    );
+  });
+  
   return;
 };
 
