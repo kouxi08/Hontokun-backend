d898142edc840e794299df9bfc7e4dea7113e958
kouxi08 <kouxi14@gmail.com>
2024-12-12 14:26:53 +0900

    [update] refs 24 build&pushできるようにした

--- src/core/converter/quiz.ts
+++ src/core/converter/quiz.ts
@@ -1,19 +1,19 @@
-import type { Quiz } from '../../model/quiz/quiz';
-import type { components as OpenapiComponents } from '../../openapi/schema';
+// import type { Quiz } from '../../model/quiz/quiz';
+// import type { components as OpenapiComponents } from '../../openapi/schema';
 
-type OpenAPIQuiz = OpenapiComponents['schemas']['Quiz'];
+// type OpenAPIQuiz = OpenapiComponents['schemas']['Quiz'];
 
-export const convertQuizToAPI = (quiz: Quiz): OpenAPIQuiz => {
-  return {
-    id: quiz.id.value,
-    news: {
-      title: quiz.news.title,
-      content: quiz.news.content,
-      image: quiz.news.image,
-    },
-    question: quiz.question,
-    type: quiz.type,
-    choices: quiz.choices,
-    correctAnswer: quiz.correctAnswer,
-  };
-};
+// export const convertQuizToAPI = (quiz: Quiz): OpenAPIQuiz => {
+//   return {
+//     id: quiz.id.value,
+//     news: {
+//       title: quiz.news.title,
+//       content: quiz.news.content,
+//       image: quiz.news.image,
+//     },
+//     question: quiz.question,
+//     type: quiz.type,
+//     choices: quiz.choices,
+//     correctAnswer: quiz.correctAnswer,
+//   };
+// };
