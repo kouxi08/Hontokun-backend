26dae3ff3fd56430a49e651b5b9ef47f658b1cf2
shiorioz <ozws011.tmt@gmail.com>
2025-01-09 05:43:23 +0000

    [fix] refs 32 バリデーション修正

--- src/database/mysql/validators/userValidator.ts
+++ src/database/mysql/validators/userValidator.ts
@@ -3,22 +3,22 @@ import { z } from 'zod';
 import { usersTable } from '../schema/schema';
 
 export const insertUserSchema = createInsertSchema(usersTable, {
-  firebase_uid: z.string().min(1).max(128),
+  firebaseUid: z.string().min(1).max(128),
   nickname: z.string().min(1).max(20),
   birthday: z.date().optional(),
   level: z.number().int().min(1),
   experience: z.number().int().min(0),
-  costume_id: z.string().uuid().optional(),
+  costumeId: z.string().uuid().optional(),
 });
 
 export const selectUserSchema = createSelectSchema(usersTable, {
   id: z.string().uuid(),
-  firebase_uid: z.string().min(1).max(128),
+  firebaseUid: z.string().min(1).max(128),
   nickname: z.string().min(1).max(20),
   birthday: z.date().optional(),
   level: z.number().int().min(1),
   experience: z.number().int().min(0),
-  costume_id: z.string().uuid().optional(),
-  created_at: z.date(),
-  updated_at: z.date(),
+  costumeId: z.string().uuid().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
