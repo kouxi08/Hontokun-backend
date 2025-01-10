58d1bc83973025b59f173bbd826f2a3d7f4f5432
shiorioz <ozws011.tmt@gmail.com>
2025-01-10 01:39:29 +0000

    [add] refs 32 dbのバリデーション修正

--- src/database/mysql/validators/userValidator.ts
+++ src/database/mysql/validators/userValidator.ts
@@ -1,24 +1,24 @@
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 import { usersTable } from '../schema/schema';
+import { idRegex } from '../../../core/regex';
 
-export const insertUserSchema = createInsertSchema(usersTable, {
-  firebaseUid: z.string().min(1).max(128),
-  nickname: z.string().min(1).max(20),
-  birthday: z.date().optional(),
-  level: z.number().int().min(1),
-  experience: z.number().int().min(0),
-  costumeId: z.string().uuid().optional(),
-});
-
-export const selectUserSchema = createSelectSchema(usersTable, {
+const userSchema = {
   id: z.string().uuid(),
   firebaseUid: z.string().min(1).max(128),
   nickname: z.string().min(1).max(20),
   birthday: z.date().optional(),
   level: z.number().int().min(1),
   experience: z.number().int().min(0),
-  costumeId: z.string().uuid().optional(),
+  costumeId: z.string().regex(idRegex),
   createdAt: z.date(),
   updatedAt: z.date(),
+}
+
+export const insertUserSchema = createInsertSchema(usersTable, { ...userSchema })
+  .omit({ createdAt: true, updatedAt: true })
+  .required({ firebaseUid: true, nickname: true, level: true, experience: true, costumeId: true });
+
+export const selectUserSchema = createSelectSchema(usersTable, {
+  ...userSchema,
 });
