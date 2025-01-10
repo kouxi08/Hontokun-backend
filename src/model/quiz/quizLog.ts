c8d75a417c02cfde99a7beeeedc39f2dbf5c4b77
shiorioz <ozws011.tmt@gmail.com>
2025-01-10 07:33:25 +0000

    [add] refs 14 クイズログの生成処理を追加

--- src/model/quiz/quizLog.ts
+++ src/model/quiz/quizLog.ts
@@ -1,27 +1,10 @@
-type QuizLogParams = {
+type QuizLog = {
   id: string;           // クイズログID
   quizId: string;       // クイズID
   quizSetLogId: string; // クイズセットログID
   userAnswer: string;   // ユーザーの回答
   time: number;         // 解答時間（秒数）
   isCorrect: boolean;   // 正誤フラグ
-  createdAt: Date;      // 作成日時
-  updatedAt: Date;      // 更新日時
+  createdAt?: Date;      // 作成日時
+  updatedAt?: Date;      // 更新日時
 };
-
-export class QuizLog {
-  private constructor(private params: QuizLogParams) { }
-
-  public static create(params: QuizLogParams): QuizLog {
-    return new QuizLog({ ...params });
-  }
-
-  public getQuizId(): string {
-    return this.params.quizId;
-  }
-
-  public isAnswerCorrect(): boolean {
-    return this.params.isCorrect;
-  }
-
-}
--- src/usecase/quizLog.ts
+++ src/usecase/quizLog.ts
@@ -1,27 +1,38 @@
 import { MySql2Database } from "drizzle-orm/mysql2";
 import * as QuizRepository from "../repository/quiz";
+import * as QuizLogRepository from "../repository/quizLog";
+import * as QuizModeRepository from "../repository/quizMode";
 
 export type Answer = {
   quizId: string,
   answer: string,
   answerTime: number,
+  isCorrect?: boolean,
 }
 
 export const createQuizLog = async (
   db: MySql2Database,
   userId: string,
+  quizMode: string,
   answers: Answer[],
 ): Promise<void> => {
   for (const answer of answers) {
     // クイズデータをDBから取得
-    const quiz = await QuizRepository.getQuizById(db, answer.quizId);
-    console.log(JSON.stringify(quiz, null, 2));
-    if (!quiz) {
+    const quizData = await QuizRepository.getQuizById(db, answer.quizId);
+    if (!quizData) {
       throw new Error('Quiz not found');
     }
 
-    // TODO: DBにquizLogを保存
+    // 回答が正しいか判定
+    answers.forEach((answer) => {
+      answer.isCorrect = answer.answer === quizData.answer;
+    })
+
+    // DBにログを保存
+    const modeId = await QuizModeRepository.getQuizModeId(db, quizMode);
+    const response = await QuizLogRepository.createQuizLog(db, userId, modeId, answers);
     // TODO: 正解したらポイント加算(tierの数を加算、1レベル10ポイント)
   }
+  // TODO: クイズのデータを返却
   return;
 }
