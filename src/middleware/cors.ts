478b10c3b63a051aec275581fcd00275864db8e6
shiorioz <ozws011.tmt@gmail.com>
2025-01-23 10:12:46 +0000

    [fix] corsの設定を修正

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,9 +1,24 @@
 import { cors } from 'hono/cors';
+import { APP_BASE_URL } from '../config/env.js';
 
 export const corsMiddleware = () => {
   return cors({
-    origin: '*',
-    allowMethods: ['POST', 'OPTIONS'],
+    origin: [APP_BASE_URL, 'http://localhost:3000'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'Content-Type'],
     allowHeaders: ['Content-Type'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
   });
 };
+
+export const corsMiddlewareForMicroCMS = () => {
+  return cors({
+    origin: '*',
+    allowMethods: ['POST', 'OPTIONS',],
+    allowHeaders: ['Content-Type'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+}
