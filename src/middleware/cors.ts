a799151d3f5a339b13888db3d698acef2f174edf
shiorioz <ozws011.tmt@gmail.com>
2025-01-24 01:41:17 +0000

    [fix] refs 90 authヘッダー許可追加

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,7 +5,7 @@ export const corsMiddleware = () => {
   return cors({
     origin: [APP_BASE_URL, 'http://localhost:8080'],
     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'Content-Type'],
-    allowHeaders: ['Content-Type'],
+    allowHeaders: ['Content-Type', 'authorization'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
     maxAge: 600,
     credentials: true,
